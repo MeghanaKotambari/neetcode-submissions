495069fa57ee79cee64e483cdb7406dac91cb620
MeghanaKotambari <meghanakotambari@gmail.com>
2026-08-21 22:18:57 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.py
@@ -0,0 +1,16 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.set = [False] * 1_000_001
+        
+
+    def add(self, key: int) -> None:
+        self.set[key] = True
+        
+
+    def remove(self, key: int) -> None:
+        self.set[key] = False
+        
+
+    def contains(self, key: int) -> bool:
+        return self.set[key]
